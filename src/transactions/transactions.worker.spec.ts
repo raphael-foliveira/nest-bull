a604d648e92602e0eb54612a5a156d372b6d6936
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-02-12 09:19:17 -0300

    tests: create worker tests

---
+++ src/transactions/transactions.worker.spec.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect, beforeEach, spyOn } from 'bun:test';
+import type { TransactionsRepository } from './transactions.repository';
+import { TransactionsWorker } from './transactions.worker';
+import type { CreateTransactionDto } from './dto/create-transaction.dto';
+import type { Job } from 'bull';
+import type { Transaction } from './entities/transaction.entity';
+
+describe('TransactionsWorker', () => {
+  let transactionsWorker: TransactionsWorker;
+  let transactionsRepository: TransactionsRepository;
+
+  beforeEach(() => {
+    transactionsRepository = {
+      findById: () => {},
+      create: () => {},
+    } as any;
+    transactionsWorker = new TransactionsWorker(transactionsRepository);
+  });
+
+  describe('process', () => {
+    it('should successfuly save a transaction when it is not already present in the database', async () => {
+      spyOn(transactionsRepository, 'findById').mockImplementationOnce(
+        async () => null
+      );
+      const result = await transactionsWorker.process({
+        id: 1,
+        data: { userId: 1, amount: 100 },
+      } as Job<CreateTransactionDto>);
+      expect(result).toBe('ok');
+    });
+
+    it("should fail to save a transaction when it's already present in the database", async () => {
+      spyOn(transactionsRepository, 'findById').mockImplementationOnce(
+        async (id: number) =>
+          ({
+            id,
+          }) as Transaction
+      );
+      const result = await transactionsWorker.process({
+        id: 1,
+        data: { userId: 1, amount: 100 },
+      } as Job<CreateTransactionDto>);
+      expect(result).toBe('failed');
+    });
+  });
+});
