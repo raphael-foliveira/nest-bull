5cfbde6667f6f904d04e584971e35f104895c8c4
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-01 17:58:38 -0300

    refactor: wait for job to finish before returning from create method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { WorkersModule } from './workers/workers.module';
 
 @Module({
-  imports: [SharedModule, TransactionsModule],
+  imports: [WorkersModule, SharedModule, TransactionsModule],
 })
 export class AppModule {}
--- src/workers/workers.module.ts
+++ src/workers/workers.module.ts
@@ -1,12 +1,10 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { RepositoryModule } from '../repository/repository.module';
-import { SharedModule } from '../shared/shared.module';
 import { TransactionsWorker } from './transactions.worker';
 
 @Module({
   imports: [
-    SharedModule,
     BullModule.registerQueue({ name: 'transactions' }),
     RepositoryModule,
   ],
